693f8908fcc1c4488b8e05cb31e740e883a903e0
hungry-matt <yoodjin2@gmail.com>
2020-08-30 01:20:32 +0900

    change : downgrade 2.2.1.RELEASE

---
+++ demo-api/src/test/java/com/example/demo/interfaces/domain/RestaurantTests.java
@@ -0,0 +1,24 @@
+package com.example.demo.interfaces.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class RestaurantTests {
+
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant("Bob zip");
+
+        assertThat(restaurant.getName(), is("Bob zip"));
+    }
+
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        assertThat(restaurant.getAddress(), is("Seoul"));
+        assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+    }
+
+}
