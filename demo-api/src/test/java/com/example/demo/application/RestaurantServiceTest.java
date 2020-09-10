f33c5da404a5f08025998526f098af8321a17619
matt <yoodjin2@gmail.com>
2020-09-10 13:10:01 +0900

    add: MockBean 활용한 테스트 코드 작성

--- demo-api/src/test/java/com/example/demo/application/RestaurantServiceTest.java
+++ demo-api/src/test/java/com/example/demo/application/RestaurantServiceTest.java
@@ -3,33 +3,48 @@ package com.example.demo.application;
 import com.example.demo.domain.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
 
+@RunWith(SpringRunner.class)
 public class RestaurantServiceTest {
 
+//    private RestaurantService restaurantService;
+//
+//    private RestaurantRepository restaurantRepository;
+//
+//    private MenuItemRepository menuItemRepository;
+//
+//    private MenuRepository menuRepository;
+
+    @MockBean
     private RestaurantService restaurantService;
 
-    private RestaurantRepository restaurantRepository;
-
-    private MenuItemRepository menuItemRepository;
-
-    private MenuRepository menuRepository;
-
-    @Before
-    public void setUp() {
-        menuItemRepository = new MenuItemRepositoryImpl();
-        restaurantRepository = new RestaurantRepositoryImpl();
-        menuRepository = new MenuRepositoryImpl();
-        restaurantService = new RestaurantService(restaurantRepository, menuItemRepository, menuRepository);
-    }
+//    @Before
+//    public void setUp() {
+//        menuItemRepository = new MenuItemRepositoryImpl();
+//        restaurantRepository = new RestaurantRepositoryImpl();
+//        menuRepository = new MenuRepositoryImpl();
+//        restaurantService = new RestaurantService(restaurantRepository, menuItemRepository, menuRepository);
+//    }
 
     @Test
     public void getRestaurant() {
-        Restaurant restaurant = restaurantService.getRestaurant(1004L);
+
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+
+        restaurant.addMenuItem(new MenuItem("kimchi"));
+
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant);
+
+//        restaurant = restaurantService.getRestaurant(1004L);
 
         assertThat(restaurant.getId(), is(1004L));
 
