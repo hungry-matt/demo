219f802f07c70748953a8d478af5c958901d76bf
hungry-matt <yoodjin2@gmail.com>
2020-10-11 23:15:25 +0900

    add: region (amdin, customer) 조회, 등록 기능 추가

--- demo-customer-api/src/main/java/com/example/demo/interfaces/RegionController.java
+++ demo-customer-api/src/main/java/com/example/demo/interfaces/RegionController.java
@@ -9,11 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.xml.stream.Location;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
