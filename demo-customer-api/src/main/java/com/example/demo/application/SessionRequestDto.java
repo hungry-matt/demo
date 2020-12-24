ef339a294113c85d83fe9c200b536d2e84bb581c
matt <yoodjin2@gmail.com>
2020-12-24 16:22:30 +0900

    change: 요청값을 받을때  @Builder 사용으로 public constructor가 없어 객체를 생성하지 못하는 오류 발생

--- demo-customer-api/src/main/java/com/example/demo/application/SessionRequestDto.java
+++ demo-customer-api/src/main/java/com/example/demo/application/SessionRequestDto.java
@@ -1,13 +1,12 @@
 package com.example.demo.application;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class SessionRequestDto {
 
     private String email;
 
     private String password;
+
 }
