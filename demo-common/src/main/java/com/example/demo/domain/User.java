095412e8203049debfb632e870b212ac3d0b8d27
matt <yoodjin2@gmail.com>
2020-12-28 02:41:20 +0900

    change: 불필요 기능 삭제

--- demo-common/src/main/java/com/example/demo/domain/User.java
+++ demo-common/src/main/java/com/example/demo/domain/User.java
@@ -46,10 +46,4 @@ public class User {
         this.level = 0L;
     }
 
-    @JsonIgnore
-    //json으로 결과를 리턴할때 accessToken을 일괄적으로 전달하지 않기.
-    //user 정보 조회시 password 데이터가 없으면 NPE 발생.
-    public String getAccessToken() {
-        return password == null ? "" : password.substring(0, 10);
-    }
 }
