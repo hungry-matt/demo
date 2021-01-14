bcc5ef2685fdfd0d0ba41b0dcd6e33a0b125060e
matt <yoodjin2@gmail.com>
2021-01-14 18:45:55 +0900

    add: token에서 name 조회

--- demo-customer-api/src/main/java/com/example/demo/interfaces/UserController.java
+++ demo-customer-api/src/main/java/com/example/demo/interfaces/UserController.java
@@ -2,12 +2,11 @@ package com.example.demo.interfaces;
 
 import com.example.demo.application.UserService;
 import com.example.demo.domain.User;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -28,5 +27,14 @@ public class UserController {
 
         String url = "/users/" + user.getId();
         return ResponseEntity.created(new URI(url)).body("{}");
+    }
+
+    @GetMapping("/me")
+    public String me(Authentication authentication) {
+        Claims claims = (Claims) authentication.getPrincipal();
+
+        String name = claims.get("name", String.class);
+
+        return name;
     }
 }
