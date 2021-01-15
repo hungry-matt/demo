b434454b6eda0b8462574b4c32bff4eca398f588
matt <yoodjin2@gmail.com>
2021-01-15 17:22:20 +0900

    change: import 어노테이션 묶음

--- demo-login-api/src/main/java/com/example/demo/interfaces/SessionController.java
+++ demo-login-api/src/main/java/com/example/demo/interfaces/SessionController.java
@@ -5,10 +5,7 @@ import com.example.demo.domain.User;
 import com.example.demo.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
